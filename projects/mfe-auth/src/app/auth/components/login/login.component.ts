8369bedb919a3a9b172491ad27310661f3902378
Edgar Mejia <edgarinhm@hotmail.com>
2025-05-04 18:42:54 -0500

    add login logic

--- projects/mfe-auth/src/app/auth/components/login/login.component.ts
+++ projects/mfe-auth/src/app/auth/components/login/login.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { CommonsLibService, IUser } from '@commons-lib';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  private readonly router = inject(Router);
+  commonsLibService = inject(CommonsLibService);
 
+  loginForm = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/[a-z0-9\._%\+\-]+@[a-z0-9\.\-]+\.[a-z]{2,}$/),
+    ]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  onSubmit() {
+    const user = { ...this.loginForm.value };
+    this.commonsLibService.signIn(user as IUser);
+    this.router.navigate(['/']);
+  }
 }
